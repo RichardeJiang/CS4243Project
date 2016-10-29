2528734e93ca67fab5bb2d961e092f0add5bb337
DQLin <dqyes2008@gmail.com>
2016-10-29 18:32:00 +0800

    update warp.py

--- warp.py
+++ warp.py
@@ -1,6 +1,9 @@
 import numpy as np
 import cv2
 
+uoy = 126.
+loy = 105.
+lox = 1439.
 
 def findHomographyMatrix(src_pts, dst_pts):
 	# ptFrame1 is 3D numpy array, each element is 2D array of the coordinate
@@ -10,22 +13,27 @@ def findHomographyMatrix(src_pts, dst_pts):
 		homographyMatrix, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
 		return homographyMatrix
 
-def warpTwoImages(img1, img2, H):
+def warpTwoImages(img1, img2, H, cutp):
     '''warp img2 to img1 with homograph matrix H'''
     height1,width1 = img1.shape[:2]
     height2,width2 = img2.shape[:2]
     p1 = np.float32([[0,0],[0,height1],[width1,height1],[width1,0]]).reshape(-1,1,2)
     p2 = np.float32([[0,0],[0,height2],[width2,height2],[width2,0]]).reshape(-1,1,2)
     p3 = cv2.perspectiveTransform(p2, H)
-
+    pp = cv2.perspectiveTransform(cutp, H)
+    xcut = pp[0,0,0]
+    ycut = pp[0,0,1] 
     #combint two images and reshape the image size
     points = np.concatenate((p1, p3), axis=0)
-    [xmin, ymin] = np.int32(points.min(axis=0).ravel() - 0.5)
-    [xmax, ymax] = np.int32(points.max(axis=0).ravel() + 0.5)
+	
+    [xmin, ymin] = [xcut-lox, ycut-loy]
+    [xmax, ymax] = [xcut, ycut+uoy]
+    #[xmin, ymin] = np.int32(points.min(axis=0).ravel() - 0.5)
+    #[xmax, ymax] = np.int32(points.max(axis=0).ravel() + 0.5)
     M = np.array([[1,0,-xmin],[0,1,-ymin],[0,0,1]])
 
-    outputImg = cv2.warpPerspective(img2, M.dot(H), (xmax-xmin, ymax-ymin))
-    outputImg[-ymin:height1-ymin,-xmin:width1-xmin] = img1
+    outputImg = cv2.warpPerspective(img2, M.dot(H), (int(xmax-xmin), int(ymax-ymin)))
+    #outputImg[-ymin:height1-ymin,-xmin:width1-xmin] = img1
     return outputImg
 
 
@@ -50,7 +58,8 @@ ret, old_frame = cap.read()
 old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
 p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
 print p0
-p0 = np.float32(np.array([[[36.,135.]],[[294.,80.]],[[122.,185.]],[[224.,70.]],[[172.,206.]],[[134.,158.]],[[60.,142.]],[[80.,170.]],[[206.,112.]]]))
+p0 = np.float32(np.array([[[36.,135.]],[[294.,80.]],[[122.,185.]],[[224.,70.]],[[172.,206.]],[[134.,158.]],[[60.,142.]],[[80.,170.]],[[95.,218.]],[[27.,218.]],[[45.,186.]],[[28.,264.]],
+[[354., 69.]],[[230.,43.]]]))
 
 #[[440.,138.]],[[310.,78.]],[[494.,188.]],[[202.,290.]]
 print p0
@@ -58,13 +67,18 @@ print p0
 # Create a mask image for drawing purposes
 mask = np.zeros_like(old_frame)
 
-pano = np.float32(np.array([[[518.,267.]],[[458.,660.]],[[141.,277.]],[[142.,168.]]]))
+cutp = np.float32(np.array([[[631.,299.]]]))
+pano = np.float32(np.array([[[518.,267.]],[[458.,163.]],[[141.,277.]],[[142.,168.]]]))
 orin = np.float32(np.array([[[202.,288.]],[[440.,138.]],[[80.,134.]],[[282.,86.]]]))
 pimg = cv2.imread('pano.png')
 curr = orin
 count = 0
-while(1):
+results = []
+
+while(ret):
     ret,frame = cap.read()
+    if (not ret):
+        break
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # calculate optical flow
@@ -77,10 +91,12 @@ while(1):
     hi = findHomographyMatrix(good_old, good_new)
     curr = cv2.perspectiveTransform(curr, hi)
     hg = findHomographyMatrix(curr, pano)
-    fimg = warpTwoImages(pimg, frame, hg)
-    cv2.imwrite('fimg'+ str(count) + '.png', fimg)
+    fimg = warpTwoImages(pimg, frame, hg, cutp)
+    results.append(fimg)
+    #if count % 100 == 0:
+        #cv2.imwrite('fimgcut'+ str(count) + '.png', fimg)
     #cv2.imshow('frame',fimg)
-    #k = cv2.waitKey(1000) & 0xff
+    #k = cv2.waitKey(1) & 0xff
     #if k == 27:
     #    break
 
@@ -88,6 +104,12 @@ while(1):
     old_gray = frame_gray.copy()
     p0 = good_new.reshape(-1,1,2)
     count += 1
+
+height,width = fimg.shape[:2]
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v') # note the lower case
+video = cv2.VideoWriter('panorama.mov',fourcc,fps=59,frameSize=(width,height),isColor=1)
+for warp in results:
+    video.write(warp)
 
 cv2.destroyAllWindows()
 cap.release()
