69330355cb722ba0af6c595fcabb84232a813d0b
zlheui <zlheui2@gmail.com>
2016-11-11 17:30:37 +0800

    rename the variable names in stitchVideo.py

--- stitchVideo/stitchVideo.py
+++ stitchVideo/stitchVideo.py
@@ -1,3 +1,8 @@
+# works with opencv 2.4.9
+# output the panorama of the videos
+# Author: Zhu Lei
+# Email: zlheui2@gmail.com
+
 from pyimagesearch.panorama import Stitcher
 import imutils
 import cv2
@@ -11,28 +16,28 @@ for i in range(0, len(filenames)):
 	cap = cv2.VideoCapture(filenames[i])
 	print "Frame Count:", cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
 	print "Frame per second:", cap.get(cv2.cv.CV_CAP_PROP_FPS)
-	frame_count = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
+	frameCount = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT))
 	fps = int(cap.get(cv2.cv.CV_CAP_PROP_FPS))
 
-	w=int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
-	h=int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+	w = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+	h = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
 	fourcc = cv2.cv.CV_FOURCC(*'XVID')
 	out = cv2.VideoWriter(ofile[i], fourcc, fps, (w*3, 2*h))
-	ret, old_frame = cap.read()
+	ret, oldFrame = cap.read()
 
-	x,y,z = old_frame.shape
+	x,y,z = oldFrame.shape
 	bg = np.zeros(shape=(2*x, 3*y, z), dtype=np.uint8)
-	bg[x/2:x+x/2, y:2*y] = old_frame
+	bg[x/2:x+x/2, y:2*y] = oldFrame
 
-	frame_aug = np.zeros(shape=(2*x, 3*y, z), dtype=np.uint8)
+	frameAug = np.zeros(shape=(2*x, 3*y, z), dtype=np.uint8)
 	stitcher = Stitcher()
-	for fr in range(1, frame_count):
+	for fr in range(1, frameCount):
 		ret, frame = cap.read()
-		frame_aug.fill(0)
-		frame_aug[x/2:x+x/2, y:2*y] = frame
+		frameAug.fill(0)
+		frameAug[x/2:x+x/2, y:2*y] = frame
 
 		print fr, bg.shape
-		result = stitcher.stitch([frame_aug, bg])
+		result = stitcher.stitch([frameAug, bg])
 		out.write(result)
 		
 		cv2.imshow("out", result)
